0d5faf0514d11417975d44c57a530cd7bf010f50
Abdullah-ShahAlam-Dev <abdullahshahalam2003@gmail.com>
2026-07-14 23:58:21 +0500

    SMTP Verification Fixed

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -37,13 +37,15 @@ const sendOtpEmail = async (mail, otp, purpose = 'account verification') => {
     pass: process.env.PORTAL_PASSWORD
   }
 });
-  
-  const mailOptions = {
-    from: process.env.PORTAL_EMAIL,
-    to: mail,
-    subject: 'MaintainIQ — OTP Verification',
-    text: `Your OTP for ${purpose} is: ${otp}\n\nThis code expires shortly. If you did not request this, you can ignore this email.`
-  };
+
+
+const mailOptions = {
+  from: process.env.PORTAL_EMAIL,
+  to: mail,
+  subject: 'MaintainIQ — OTP Verification',
+  text: `Your OTP for ${purpose} is: ${otp}\n\nThis code expires shortly. If you did not request this, you can ignore this email.`
+};
+console.log('[Nodemailer] OTP SENT TO EMAIL Sucessfully');
 
   try {
     const info = await transporter.sendMail(mailOptions);
