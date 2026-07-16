6e92117ab38f27a165fccc4a936389242385dfe6
Abdullah-ShahAlam-Dev <abdullahshahalam2003@gmail.com>
2026-07-16 20:00:40 +0500

    ADDED: RBAC for asset edit/delete and handle orphaned issues safely

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 // login is sent automatically by the browser as long as withCredentials
 // is true. There is no token to attach manually anymore.
 const api = axios.create({
-  // baseURL: 'https://maintainiq-hackathon.onrender.com/api',
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://maintainiq-hackathon.onrender.com/api',
+  // baseURL: 'http://localhost:5000/api',
   withCredentials: true
 });
 
